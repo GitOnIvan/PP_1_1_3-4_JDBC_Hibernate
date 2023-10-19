aa30424e76fe6802ac96c80f8a538bf21491abea
Ivan Onyfreichyk <138799282+GitOnIvan@users.noreply.github.com>
2023-10-19 19:28:01 +0500

    Util realisation was added.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static String URL; // = "jdbc:mysql://localhost:3306/pp_db";
+    private static String USERNAME; // = "root";
+    private static String PASSWORD; // = "Kris07102023";
+
+    public static Connection getCon() {
+
+
+
+
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection (URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    return connection;
+
+    }
+
+
+
+
+
 }
---
+++ src/test/resources/PP.sql
@@ -0,0 +1,6 @@
+#insert into pp (name, lastname, age) values ('John', 'Smith', 25);
+#insert into pp values (DEFAULT, 'John', 'Smith', 25);
+#delete from pp where id=11;
+#delete from pp where id=10;
+#select * from pp;
+#update pp set name='LOL', age=999 where id=12;
